6d478b678d8fe902eba93ce88050e9b885f2d813
Hector.perezm3144@gmail.com <hector.perezm3144@gmail.com>
2023-11-02 01:42:24 -0500

    Se crea el forulario para poder hacer las publicaciones

--- resources/views/Publicaciones/publicaciones.blade.php
+++ resources/views/Publicaciones/publicaciones.blade.php
@@ -2,5 +2,24 @@
 
 @section('title', 'publicaciones')
 @section('content')
-hola
+<div class="block mx-auto my-12 p-8 bg-white w-1/3 border border-gray-200 
+rounded-lg shadow-lg">
+
+<h1 class="text-3xl text-center font-bold">Publicacion</h1>
+
+<form class="mt-4" method="POST" action="">
+@csrf  
+<input type="text" class="border border-gray-200 rounded-md bg-gray-200 w-full
+    text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Titulo"
+    id="title" name="title">
+
+    <input type="text" class="border border-gray-200 rounded-md bg-gray-200 w-full
+    text-lg placeholder-gray-900 p-8 my-1 focus:bg-white" placeholder="Descripcion"
+    id="desc" name="desc">
+
+    <input type="date" class="border border-gray-200 rounded-md bg-gray-200 w-full
+    text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="fecha"
+    id="date" name="date">
+</form>
+</div>
 @endsection
--- resources/views/layauts/index.blade.php
+++ resources/views/layauts/index.blade.php
@@ -3,16 +3,9 @@
 @section('title', 'index')
 
 @section('content')
-<br></br>
-<div class="container border p-4 ">
-<h5 class="title">Card title</h5>
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-<br></br>
 
+
+<br></br>
 <div class=" container border p-4">
 <h2 class="card-title">Mi experiencia matando jefes en dark souls</h2>
   <div class="card-body">
@@ -20,5 +13,7 @@
     <div class="post-body">09/02/2023</div>
   </div>
 </div>
+<br></br>
+
 
 @endsection
