039d8c7d2a9e0f7013ab97b27394f50f4d5b159c
Hector.perezm3144@gmail.com <hector.perezm3144@gmail.com>
2023-11-02 01:00:53 -0500

    Se crea la pagina de publicaciones

--- resources/views/layauts/index.blade.php
+++ resources/views/layauts/index.blade.php
@@ -3,7 +3,22 @@
 @section('title', 'index')
 
 @section('content')
+<br></br>
+<div class="container border p-4 ">
+<h5 class="title">Card title</h5>
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<br></br>
 
-<h1 class="text-center">Bienvenido a la app</h1>
+<div class=" container border p-4">
+<h2 class="card-title">Mi experiencia matando jefes en dark souls</h2>
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div class="post-body">09/02/2023</div>
+  </div>
+</div>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdministController;
+use App\Http\Controllers\PublicacionController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
@@ -17,3 +18,7 @@ Route::get('/register', [RegisterController::class, 'create'])->name('register.i
 Route::post('/register', [RegisterController::class, 'regis'])->name('register.regis');
 
 Route::get('/admin', [AdministController::class, 'administ'])->middleware('admini')->name('aminist.index');
+
+Route::get('/login', [SessionController::class, 'create'])->name('login.index');
+
+Route::get('/publicacion', [PublicacionController::class, 'create'])->name('publi.index');
