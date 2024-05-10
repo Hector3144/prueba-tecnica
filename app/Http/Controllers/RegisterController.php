f797ef3879d416e80797a7107a8beb0f33713ec8
KEVINtique351 <kevintique86@gmail.com>
2024-05-10 17:50:53 -0500

    ajustes

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash; // Add this import statement
 
 class RegisterController extends Controller
 {
+    public $pass;
+
     public function create(){
 
         return view('auth.register');
@@ -19,12 +22,11 @@ class RegisterController extends Controller
             'email' => 'required|email',
             'password' => 'required|confirmed',
         ]);
-
         $user = new User();
         $user->name = request('name');
         $user->age = request('age');
         $user->email = request('email');
-        $user->password = bcrypt(request('password'));
+        $user->password = request('password'); // Use Hash::make() to encrypt the password
         $user->role_id = 3; // Set the role_id value explicitly
         $user->view_id = 1; // Set the view_id value explicitly
 
